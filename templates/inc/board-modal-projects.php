dc315a16bc12c876250bf5d0570c6a74c4ff7dd6
Corey Maass <corey@gelform.com>
2016-02-27 09:11:34 -0600

    breaking css out into scss files. breaking board out into includes. adding test tabs.

---
+++ templates/inc/board-modal-projects.php
@@ -0,0 +1,17 @@
+
+<div class="modal fade" id="modal-projects" data-keyboard="false">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<button type="button" class=" btn btn-default btn-close" data-dismiss="modal">
+					<?php echo __('Close', 'kanban') ?>
+				</button>
+
+				<div class="panel-group" id="accordion-projects">
+				</div><!-- panel-group -->
+
+
+			</div><!-- body -->
+		</div><!-- content -->
+	</div><!-- dialog -->
+</div><!-- modal -->

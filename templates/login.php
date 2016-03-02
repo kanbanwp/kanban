e96c1c354be17458398b8438795b560e8756694c
JeroenSormani <jeroen.sormani@gmail.com>
2016-03-02 23:51:00 +0100

    Code consistency changes for templates/login.php

--- templates/login.php
+++ templates/login.php
@@ -1,4 +1,4 @@
-<?php include Kanban_Template::find_template('inc/header') ?>
+<?php include Kanban_Template::find_template( 'inc/header' ); ?>
 
 
 
@@ -11,39 +11,39 @@
 
 
 
-		<?php Kanban_Flash::flash() ?>
+		<?php Kanban_Flash::flash(); ?>
 
 
 
 		<div class="panel panel-default">
 			<div class="panel-heading">
-<?php if ( !is_user_logged_in() ) : ?>
-				<h2><?php echo __('Please Login', Kanban::$instance->settings->file) ?></h2>
+<?php if ( ! is_user_logged_in() ) : ?>
+				<h2><?php echo __( 'Please Login', Kanban::$instance->settings->file ); ?></h2>
 				<div class="form-group">
 					<label for="email" class="sr-only">
-						<?php echo __('Email or username', 'kanban') ?>
+						<?php echo __( 'Email or username', 'kanban' ); ?>
 					</label>
-					<input type="text" name="email" id="email" class="form-control input-lg" placeholder="<?php echo __('Email or username', 'kanban') ?>" required autofocus>
+					<input type="text" name="email" id="email" class="form-control input-lg" placeholder="<?php echo __('Email or username', 'kanban'); ?>" required autofocus>
 				</div><!-- form group -->
 				<div class="form-group">
 					<label for="password" class="sr-only">
-						<?php echo __('Password', 'kanban') ?>
+						<?php echo __( 'Password', 'kanban' ); ?>
 					</label>
-					<input type="password" name="password" id="password" class="form-control input-lg" placeholder="<?php echo __('Password', 'kanban') ?>" required>
+					<input type="password" name="password" id="password" class="form-control input-lg" placeholder="<?php echo __('Password', 'kanban'); ?>" required>
 				</div><!-- form group -->
 				<div>
 					<button type="submit" class="btn btn-lg btn-primary btn-block">
-						<?php echo __('Log in', 'kanban') ?>
+						<?php echo __( 'Log in', 'kanban' ); ?>
 					</button>
 					<?php wp_nonce_field( 'login', Kanban_Utils::get_nonce() ); ?>
 				</div>
 <?php else : // is_user_logged_in ?>
 				<p>
-					<?php echo __('Whoops, looks like you haven\'t been granted access yet. Click below to request access.', 'kanban') ?>
+					<?php echo __( 'Whoops, looks like you haven\'t been granted access yet. Click below to request access.', 'kanban' ); ?>
 				</p>
 				<p class="text-center">
 					<button type="submit" class="btn btn-primary btn-lg">
-						<?php echo __('Request access', 'kanban') ?>
+						<?php echo __( 'Request access', 'kanban' ); ?>
 					</button>
 					<?php wp_nonce_field( 'request_access', Kanban_Utils::get_nonce() ); ?>
 				</p>
@@ -58,6 +58,4 @@
 
 
 
-<?php include Kanban_Template::find_template('inc/footer') ?>
-
-
+<?php include Kanban_Template::find_template( 'inc/footer' )

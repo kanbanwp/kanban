fd49d5ee95bbadc55d006131763ed577bc29d287
JeroenSormani <jeroen.sormani@gmail.com>
2016-03-02 23:51:14 +0100

    Code consistency changes for includes/class-task_hour.php

--- includes/class-task_hour.php
+++ includes/class-task_hour.php
@@ -24,10 +24,10 @@ class Kanban_Task_Hour extends Kanban_Db
 
 	// define db table columns and their validation type
 	protected static $table_columns = array(
-		'task_id' => 'int',
+		'task_id'        => 'int',
 		'created_dt_gmt' => 'datetime',
-		'hours' => 'float',
-		'status_id' => 'int',
+		'hours'          => 'float',
+		'status_id'      => 'int',
 		'user_id_author' => 'int',
 		'user_id_worked' => 'int'
 	);
@@ -36,26 +36,26 @@ class Kanban_Task_Hour extends Kanban_Db
 
 	static function init()
 	{
-		add_action( sprintf('wp_ajax_add_%s', self::$slug), array(__CLASS__, 'ajax_save') );
+		add_action( sprintf( 'wp_ajax_add_%s', self::$slug ), array( __CLASS__, 'ajax_save' ) );
 	}
 
 
 
-	static function ajax_save ()
+	static function ajax_save()
 	{
-		if (  !isset( $_POST[Kanban_Utils::get_nonce()] ) || ! wp_verify_nonce( $_POST[Kanban_Utils::get_nonce()], 'kanban-save') || !is_user_logged_in() ) wp_send_json_error();
+		if ( ! isset( $_POST[Kanban_Utils::get_nonce()] ) || ! wp_verify_nonce( $_POST[Kanban_Utils::get_nonce()], 'kanban-save' ) || ! is_user_logged_in() ) wp_send_json_error();
 
 
 
-		do_action( 'kanban_task_hour_ajax_save_before', $_POST['task']['id']);
+		do_action( 'kanban_task_hour_ajax_save_before', $_POST['task']['id'] );
 
 
 
-		$user_id_author = isset($_POST['user_id_author']) ? $_POST['user_id_author'] : get_current_user_id();
+		$user_id_author = isset( $_POST['user_id_author'] ) ? $_POST['user_id_author'] : get_current_user_id();
 
 
 
-		if ( empty($_POST['user_id_worked']) )
+		if ( empty( $_POST['user_id_worked'] ) )
 		{
 			$_POST['user_id_worked'] = $user_id_author;
 		}
@@ -64,42 +64,42 @@ class Kanban_Task_Hour extends Kanban_Db
 
 		try
 		{
-			$operator = substr($_POST['operator'], 0, 1) == '-' ? '-' : '+';
-			$val = sprintf('%s%s', $operator, abs(floatval($_POST['operator'])));
+			$operator = substr( $_POST['operator'], 0, 1 ) == '-' ? '-' : '+';
+			$val = sprintf( '%s%s', $operator, abs( floatval( $_POST['operator'] ) ) );
 		}
-		catch (Exception $e)
+		catch ( Exception $e )
 		{
-			wp_send_json_error(array(
-				'message' => sprintf('Error saving %s', str_replace('_', ' ', self::$slug))
-			));
+			wp_send_json_error( array(
+				'message' => sprintf( 'Error saving %s', str_replace( '_', ' ', self::$slug ) )
+			) );
 		}
 
 
 
-		eval(sprintf('$hours = 0%s;', $val));
+		eval( sprintf( '$hours = 0%s;', $val ) );
 
 
 
 		$data = array(
-			'task_id' => $_POST['task']['id'],
+			'task_id'        => $_POST['task']['id'],
 			'created_dt_gmt' => Kanban_Utils::mysql_now_gmt(),
-			'hours' => $hours,
-			'status_is' => $_POST['task']['status_id'],
+			'hours'          => $hours,
+			'status_is'      => $_POST['task']['status_id'],
 			'user_id_author' => $user_id_author,
 			'user_id_worked' => $_POST['user_id_worked']
 		);
 
-		$is_successful = self::_insert($data);
+		$is_successful = self::_insert( $data );
 
 
 
-		do_action( 'kanban_task_hour_ajax_save_after', $data);
+		do_action( 'kanban_task_hour_ajax_save_after', $data );
 
 
 
-		if ( !empty($_POST['comment']) )
+		if ( ! empty( $_POST['comment'] ) )
 		{
-			do_action('kanban_task_hour_ajax_save_before_comment');
+			do_action( 'kanban_task_hour_ajax_save_before_comment' );
 
 			Kanban_Comment::add(
 				$_POST['comment'],
@@ -107,39 +107,39 @@ class Kanban_Task_Hour extends Kanban_Db
 				$task_id
 			);
 
-			do_action('kanban_task_hour_ajax_save_after_comment');
+			do_action( 'kanban_task_hour_ajax_save_after_comment' );
 		}
 
 
 
 		if ( $is_successful )
 		{
-			wp_send_json_success(array(
-				'message' => sprintf('%s saved', str_replace('_', ' ', self::$slug))
-			));
+			wp_send_json_success( array(
+				'message' => sprintf( '%s saved', str_replace( '_', ' ', self::$slug ) )
+			) );
 		}
 		else
 		{
-			wp_send_json_error(array(
-				'message' => sprintf('Error saving %s', str_replace('_', ' ', self::$slug))
-			));
+			wp_send_json_error( array(
+				'message' => sprintf( 'Error saving %s', str_replace( '_', ' ', self::$slug ) )
+			) );
 		}
 	}
 
 
 
 	// extend parent, so it's accessible from other classes
-	static function insert ($data)
+	static function insert( $data )
 	{
-		return self::_insert($data);
+		return self::_insert( $data );
 	}
 
 
 
 	// define the db schema
-	static function db_table ()
+	static function db_table()
 	{
-		return "CREATE TABLE " . self::table_name() . " (
+		return 'CREATE TABLE ' . self::table_name() . ' (
 					id bigint(20) NOT NULL AUTO_INCREMENT,
 					task_id bigint(20) NOT NULL,
 					created_dt_gmt datetime NOT NULL,
@@ -148,14 +148,14 @@ class Kanban_Task_Hour extends Kanban_Db
 					user_id_author bigint(20) NOT NULL,
 					user_id_worked bigint(20) NOT NULL,
 					UNIQUE KEY  (id)
-				)";
+				)';
 	} // db_table
 
 
 
 	/**
 	 * get the instance of this class
-	 * @return	object	the instance
+	 * @return object the instance
 	 */
 	public static function get_instance()
 	{
@@ -174,5 +174,3 @@ class Kanban_Task_Hour extends Kanban_Db
 	private function __construct() { }
 
 }
-
-

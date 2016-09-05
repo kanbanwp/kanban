c1fcc12c76defac686b3b140dbacf4a82d811a0d
Corey Maass <corey@gelform.com>
2016-09-05 09:58:32 -0500

    Setting 2.2.1

--- kanban.php
+++ kanban.php
@@ -3,8 +3,8 @@
 Plugin Name:		Kanban for WordPress
 Plugin URI:			http://kanbanwp.com/
 Description:		A complete Kanban project management suite for WordPress.
-Version:			2.2.0
-Release Date:		September 3, 2016
+Version:			2.2.1
+Release Date:		September 5, 2016
 Author:				Gelform Inc
 Author URI:			http://gelwp.com
 License:			GPL2

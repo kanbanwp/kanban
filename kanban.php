d27b756c34ff15c22c62c1363ae51cde555625d0
Corey Maass <corey@gelform.com>
2017-02-16 11:56:34 -0500

    2.3.7

--- kanban.php
+++ kanban.php
@@ -4,8 +4,8 @@
  * Plugin Name:        Kanban Boards for WordPress
  * Plugin URI:         http://kanbanwp.com/
  * Description:        Add Kanban boards to your WordPress sites and get organized! Project management, CRM, sales tracking...
- * Version:            2.3.6
- * Release Date:       February 11, 2017
+ * Version:            2.3.7
+ * Release Date:       February 16, 2017
  * Author:             Gelform Inc
  * Author URI:         http://gelwp.com
  * License:            GPL2

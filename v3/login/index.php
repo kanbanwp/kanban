313cc59622d8466ccf5450fa249e367afb27a1ba
gelform <corey@gelform.com>
2018-06-14 06:33:47 -0400

    Redirect old version 2 url.

--- v3/login/index.php
+++ v3/login/index.php
@@ -3,7 +3,7 @@
 // Redirect old version 2 url.
 wp_redirect(
 	add_query_arg(array(
-			'redirect_to' => Kanban_Router::instance()->get_page_uri('board')
+			'redirect_to' => urlencode(Kanban_Router::instance()->get_page_uri('board'))
 		),
 		wp_login_url()
 	));

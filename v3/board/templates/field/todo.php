ca05de4144c2c9fbd4a174c70c47e19bb96628c8
gelform <corey@gelform.com>
2018-06-21 09:55:28 -0400

    Forgot about adding a new todo item.

--- v3/board/templates/field/todo.php
+++ v3/board/templates/field/todo.php
@@ -10,20 +10,23 @@
 
 		<div class="list-group">
 			<div class="list-group-item">
-					<a href="javascript:void(0);" class="todo-move-handle btn btn-empty pull-right ei ei-menu"></a>
-						<input type="checkbox" id="checkbox-1">
-						<label for="checkbox-1" class="todo-checkbox ei ei-box-empty"></label>
-						<label for="checkbox-1" class="todo-checkbox ei ei-box-checked"></label>
-						<div class="todo" contenteditable="true">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ornare purus. Ut eu ante odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin ac mi urna. Quisque maximus quam sit amet auctor laoreet. Aenean sed porttitor nisi. Aenean pretium turpis ante, et fringilla sapien congue eu.
-						</div>
+				<a href="javascript:void(0);" class="todo-move-handle btn btn-empty pull-right ei ei-menu"></a>
+				<input type="checkbox" id="checkbox-1">
+				<label for="checkbox-1" class="todo-checkbox ei ei-box-empty"></label>
+				<label for="checkbox-1" class="todo-checkbox ei ei-box-checked"></label>
+				<div class="form-control" contenteditable="true">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a ornare purus. Ut eu ante odio.
+					Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin ac mi urna. Quisque maximus quam
+					sit amet auctor laoreet. Aenean sed porttitor nisi. Aenean pretium turpis ante, et fringilla sapien
+					congue eu.
+				</div>
 			</div>
 			<div class="list-group-item">
 				<a href="javascript:void(0);" class="todo-move-handle btn btn-empty pull-right ei ei-menu"></a>
 				<input type="checkbox" id="checkbox-2">
 				<label for="checkbox-2" class="todo-checkbox ei ei-box-empty"></label>
 				<label for="checkbox-2" class="todo-checkbox ei ei-box-checked"></label>
-				<div class="todo" contenteditable="true">
+				<div class="form-control" contenteditable="true">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				</div>
 			</div>
@@ -32,9 +35,12 @@
 				<input type="checkbox" id="checkbox-3">
 				<label for="checkbox-3" class="todo-checkbox ei ei-box-empty"></label>
 				<label for="checkbox-3" class="todo-checkbox ei ei-box-checked"></label>
-				<div class="todo" contenteditable="true">
+				<div class="form-control" contenteditable="true">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 				</div>
+			</div>
+			<div class="list-group-item">
+				<div class="form-control" contenteditable="true" placeholder="Add a new item"></div>
 			</div>
 		</div>
 	</div>

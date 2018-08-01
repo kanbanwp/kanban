f21c4dca50a4eeaafd83e394453772f901f1308a
gelform <corey@gelform.com>
2018-08-01 13:09:33 -0400

    Added uglify and now minifying javascript and css.

--- v3/board/webpack.config.js
+++ v3/board/webpack.config.js
@@ -1,30 +1,35 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var webpack = require('webpack')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack');
 
 
 module.exports = {
-  entry: ['./src/init.js','./scss/init.scss'],
-  output: {
-    filename: 'js/app.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          //resolve-url-loader may be chained before sass-loader if necessary
-          use: ['css-loader', 'sass-loader']
-        })
-      }
-    ]
-  },
+	entry: ['./src/init.js', './scss/init.scss'],
+	output: {
+		filename: 'js/app.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					//resolve-url-loader may be chained before sass-loader if necessary
+					use: [
+						{loader: 'css-loader', options: {minimize: true}},
+						'sass-loader'
+					]
+				})
+			}
+		]
+	},
 	plugins: [
-      new ExtractTextPlugin('css/app.css'),
-      new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery",
-          'window.jQuery': 'jquery'
-      })
-  ]
+		new ExtractTextPlugin('css/app.css'),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			'window.jQuery': 'jquery'
+		}),
+		new UglifyJsPlugin()
+	]
 };
